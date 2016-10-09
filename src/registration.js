336bb8754b072576c971c5616b729912cc1d6611
Will Gibbins <wgibbins@pillartechnology.com>
2016-10-09 14:42:25 -0400

    First test

---
+++ src/registration.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  getTeamMembers: function() {
+    return [];
+  }
+}
--- test/registrationTest.js
+++ test/registrationTest.js
@@ -5,7 +5,6 @@ var Registration = require('./../src/registration');
 
 describe('Registration', function() {
   it('getTeamMembers should return list of members registered', function() {
-    var registration = new Registration();
-    expect(registration.getTeamMembers()).to.equal([]);
+    expect(Registration.getTeamMembers()).to.be.empty;
   });
 });
